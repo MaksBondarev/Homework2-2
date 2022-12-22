fc749c62d4282ef0cd30be49320c9f439c4139ca
Максим <MaksB1988@yandex.ru>
2022-12-22 05:23:38 +0300

    Сохранил третье задание

--- homework2.py
+++ homework2.py
@@ -28,6 +28,25 @@
 
 # Решение:
 # n = int(input('Введите число: ' ))
+
 # lst = [round((1+1/i)**i, 2) for i in range(1, n+1)]
+
 # print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 3)}')
 
+
+# 3№ Реализуйте алгоритм перемешивания списка. НЕ ИСПОЛЬЗОВАТЬ ВСТРОЕННЫЕ БИБЛИОТЕКИ SHUFFLE, 
+# максимум использование библиотеки Random для и получения случайного int
+
+# Решение:
+# import random  
+
+# list_1 = [1,2,3,4,5,6,7,8,9,10]
+# print(f'Первоначальный списк: {list_1}')
+
+
+# list_3 = list_1
+# list_2 = []
+# while len(list_3) > 0:
+#         index = random.randrange(0,len(list_3))
+#         list_2.append(list_3.pop(index))
+# print(f'Перемешанный списк:   {list_2}')
